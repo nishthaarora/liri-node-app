6973363b701b808f2f6248c83b17c5e8c3b8adc3
Nishtha <nishtha@Nishthas-MacBook-Pro.local>
2016-10-10 13:28:24 -0500

    removing keys

--- keys.js
+++
@@ -1,8 +0,0 @@
-console.log('this is loaded.');
-
-module.exports.twitterKeys = {
-  consumer_key: 'ugPBkWJ6D4sMYqXdrerGsNbMl',
-  consumer_secret: 'dTrNZK2HizbEJsg7YCT27fYe2o8uqwlV0iF4oRKbS0Y4kmPdKL',
-  access_token_key: '103849861-wbuItEGYSF1pyfssCYQTzxxf825IhE1hePVnFv1a',
-  access_token_secret: 'JUYRhG8HzmH6KWXzR3TOeNYRYrvYgcmGt1knYIYdswYGM'
-}
