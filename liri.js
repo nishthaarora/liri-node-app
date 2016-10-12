ad2083a33e657240df5a70ae3eaa630332fc43ad
Nishtha <nishtha@Nishthas-MacBook-Pro.local>
2016-10-11 20:41:00 -0500

    adding spotify api

--- liri.js
+++ liri.js
@@ -1,6 +1,6 @@
 var inquirer = require('inquirer');
 var userChoice = process.argv[2];
-var userInput = process.argv[3];
+var userInput = process.argv.slice(3,process.argv.length).join(' ');
 
 // inquirer.prompt([
 
@@ -23,22 +23,25 @@ var userInput = process.argv[3];
 // 	}
 
 // ]).then(function(answer) {
-	// switch (answer.userChoice) {
-	switch (userChoice) {
-		case 'my-tweets':
-			getTwitterTweets();
-			break;
-		case "spotify-this-song":
-			getSpotifySong();
-			break;
-		case "movie-this":
-			break;
-		case "do-what-it-says":
-			break;
-		default:
-			text = "Please select correct answer"
+// switch (answer.userChoice) {
+	function callUserChoice(userChoice, userInput) {
+switch (userChoice) {
+	case 'my-tweets':
+		getTwitterTweets();
+		break;
+	case "spotify-this-song":
+		getSpotifySong(userInput);
+		break;
+	case "movie-this":
+		getMovies(userInput);
+		break;
+	case "do-what-it-says":
+		doAsItSays();
+		break;
+	default:
+		text = "Please select correct answer"
 	}
-// });
+};
 
 function getTwitterTweets() {
 
@@ -64,34 +67,96 @@ function getTwitterTweets() {
 			console.log(error);
 		}
 
-		for(var i=0; i<tweets.length; i++) {
+		for (var i = 0; i < tweets.length; i++) {
 			var date = moment(tweets[i].created_at).format('MM/DD/YYYY');
-			console.log('date: ' + date +' tweet: ' + tweets[i].text);
+			console.log('date: ' + date + ' tweet: ' + tweets[i].text);
 		}
 	});
 }
 
 
-function getSpotifySong () {
+function getSpotifySong(userInput) {
 
 	var spotify = require('spotify');
-	// var song = [];
 
-	// 	for(var i=3; i<process.argv.length; i++) {
-	// 		song.push(process.argv[i]);
-	// }
+	spotify.search({
+		type: 'track',
+		query: userInput || '"The Sign"',
+		limit: 1
+	}, function(err, data) {
+		if (err) {
+			console.log('Error occurred: ' + err);
+			return;
+		}
 
-	// var songString = song.join('');
+		for (var i = 0; i < data.tracks.items.length; i++) {
+			console.log('\n*********************   Tracks   ************************\n')
+			console.log('Artist Name:', data.tracks.items[i].artists[0].name)
+			console.log('Song Name:', data.tracks.items[i].name)
+			console.log('Preview Link:', data.tracks.items[i].external_urls.spotify)
+			console.log('Album Name:', data.tracks.items[i].album.name);
+		}
 
-spotify.search({ type: 'track', query: userInput || 'dancing in the moonlight' }, function(err, data) {
-	console.log(data);
-    if ( err ) {
-        console.log('Error occurred: ' + err);
-        return;
-    }
-    // Do something with 'data'
-});
+
+	});
 
 }
 
+function getMovies(userInput) {
 
+	var request = require('request');
+
+	userInput = userInput || 'forrest gump';
+
+	request('http://www.omdbapi.com/?t=' + userInput + '&y=&plot=short&tomatoes=true&r=json', function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	  	var data = JSON.parse(body);
+
+	  	console.log('\n*********************   Movie Details   ************************\n');
+	  	console.log('Title:', data.Title);
+	  	console.log('Release Year:', data.Year);
+	  	console.log('IMDB Rating:', data.imdbRating);
+	  	console.log('Country:', data.Country);
+	  	console.log('Language:', data.Language);
+	  	console.log('Plot:', data.Plot);
+	  	console.log('Actors:', data.Actors);
+	  	console.log('Rotten Tomatoes Rating:', data.tomatoRating);
+	  	console.log('Rotten Tomatoes Rating:', data.tomatoURL);
+	  }
+	})
+}
+
+
+function doAsItSays () {
+
+	var fs = require('fs');
+
+		// fs.writeFile('random.txt', 'I Want it That Way', (err) => {
+		// 	if(err) throw err;
+		// 	console.log('Saved');
+		// 	return;
+		// })
+
+		fs.readFile('random.txt', {encoding: 'utf8'}, (err, data) => {
+			if(err) throw err;
+			console.log(data);
+			var textInput = data.split(',');
+
+			callUserChoice( textInput[0], textInput[1] );
+
+		})
+
+// 		for(var i=3; i<process.argv.length; i++) {
+
+// 		fs.appendFile('random.txt\n', process.argv[i] , (err, data) => {
+// 			if(err) throw err;
+// 			console.log(data);
+// 			if(data === 'inception') {
+// 				getMovies();
+// 			}
+// 		})
+// }
+
+}
+
+callUserChoice( userChoice, userInput);
